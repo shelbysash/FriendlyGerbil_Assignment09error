0455fbc16bd194c6663b86d4c74619d2e0856cd6
Andrew Mehlman <mehlmadm@mail.uc.edu>
2024-11-04 17:34:55 -0500

    code

--- FriendlyGerbil_Assignment09/mainPackage/main.py
+++ FriendlyGerbil_Assignment09/mainPackage/main.py
@@ -3,6 +3,9 @@
 from dbUtilitiesPackage.dbUtilities import *
 
 import pyodbc
+
+
+
 try:
     conn= connect_to_database() 
     # Submit a query to the SQL Server instance and store the results in the cursor object
@@ -13,3 +16,18 @@ except Exception as e:
     print("error accessing database")
     print(e) 
     exit()  #given up. how do i get out of this module 
+
+
+#step 1    
+query_string = "SELECT ProductID, [UPC-A ], Description, ManufacturerID, BrandID FROM tProduct"
+#print(query_string)
+#Submit the query to out db server and store the results in a variable
+results = cursor.execute(query_string)
+products = cursor.fetchall()  #maybe ?
+
+#step 2
+selectedProduct = random.choice(products)
+productID = selectedProduct.ProductID
+description = selectedProduct.Description
+manufacturerID = selectedProduct.ManufacturerID
+brandID = selectedProduct.BrandID
